9717124db8e951e854c44af1e0219b6eed341956
Dogan Gunes <dogangns.98@gmail.com>
2023-01-12 15:38:08 +0300

    .

---
+++ grpcClient/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Grpc.Net.Client;
+using grpcServer;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5023");
+var greetClient = new Greeter.GreeterClient(channel);
+
+HelloReply result =  await greetClient.SayHelloAsync(new HelloRequest{
+    Name = "Dogan Gunes'den selamlar"
+});
+
+Console.WriteLine(result.Message);
+
+
+
+
